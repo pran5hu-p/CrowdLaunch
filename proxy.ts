4e6fa3be1c7c78d502602e8689761a5edfaaf391
Pranshu Pallav <pallavpranshu@gmail.com>
2026-06-17 01:18:06 +0530

    added organizations option for users

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,42 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkClient, clerkMiddleware } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware()
+export default clerkMiddleware(async(auth) => {
+  const session = await auth();
+  const {userId, orgId} = session;
+  if(userId && !orgId){
+    try{
+      const client = await clerkClient();
+      const { data: organizations } =
+        await client.users.getOrganizationMembershipList({ userId: userId });
+
+      if (organizations && organizations.length > 0) {
+        return NextResponse.next();
+      }
+
+      const user = await client.users.getUser(userId);
+
+      const orgName = user.fullName
+        ? `${user.fullName}'s Organization`
+        : user.firstName
+        ? `${user.firstName}'s Organization`
+        : user.username
+        ? `${user.username}'s Organization`
+        : user.primaryEmailAddress?.emailAddress
+        ? `${user.primaryEmailAddress?.emailAddress}'s Organization`
+        : "My Organization";
+
+      await client.organizations.createOrganization({
+        name: orgName,
+        createdBy: userId,
+      });
+      console.log(`Organization created for user ${userId} with name "${orgName}"`);
+    } catch(err) {
+        console.error("Error creating organization:", err);
+    }
+  }
+  return NextResponse.next();
+})
 
 export const config = {
   matcher: [
